0632fa7833477294f28c2800964b2ceb6cfca8ec
ted <ted10401@gmail.com>
2019-03-07 17:43:20 +0800

    First update.

---
+++ Assets/NavMeshComponents/Scripts/JSLs/NavMeshUtils.cs
@@ -0,0 +1,34 @@
+﻿
+namespace UnityEngine.AI
+{
+    public class NavMeshUtils
+    {
+        private static NavMeshPath m_navMeshPath = new NavMeshPath();
+        private static NavMeshHit m_navMeshHit;
+        public static bool CalculatePath(Vector3 sourcePosition, Vector3 targetPosition, int areaMask, out Vector3 destination)
+        {
+            destination = targetPosition;
+
+            NavMesh.CalculatePath(sourcePosition, destination, areaMask, m_navMeshPath);
+            if (m_navMeshPath.status != NavMeshPathStatus.PathComplete)
+            {
+                GetValidPosition(sourcePosition, targetPosition, areaMask, out destination);
+                NavMesh.CalculatePath(sourcePosition, destination, areaMask, m_navMeshPath);
+
+                return m_navMeshPath.status == NavMeshPathStatus.PathComplete;
+            }
+
+            return true;
+        }
+
+        public static void GetValidPosition(Vector3 sourcePosition, Vector3 targetPosition, int areaMask, out Vector3 validPosition)
+        {
+            validPosition = targetPosition;
+
+            if (NavMesh.Raycast(sourcePosition, targetPosition, out m_navMeshHit, areaMask))
+            {
+                validPosition = m_navMeshHit.position;
+            }
+        }
+    }
+}
---
+++ Assets/NavMeshComponents/Scripts/JSLs/RuntimeNavMeshSurface.cs
@@ -0,0 +1,57 @@
+﻿
+namespace UnityEngine.AI
+{
+    [RequireComponent(typeof(NavMeshSurface))]
+    public class RuntimeNavMeshSurface : MonoBehaviour
+    {
+        [SerializeField] private Transform m_targetAgent;
+        [SerializeField] private float m_updateDistance = 1;
+
+        private NavMeshSurface m_navMeshSurface;
+        private Transform m_transform;
+        private Vector3 m_lastBakePosition;
+        private float m_cacheDistance;
+        private bool m_initialized;
+
+        private void Awake()
+        {
+            m_transform = transform;
+            m_transform.SetParent(m_targetAgent.parent);
+            m_navMeshSurface = GetComponent<NavMeshSurface>();
+
+            Bake();
+        }
+
+        public void Initialize()
+        {
+            if(m_initialized)
+            {
+                return;
+            }
+
+            m_initialized = true;
+            Bake();
+        }
+
+        private void Update()
+        {
+            m_cacheDistance = Vector3.Distance(m_targetAgent.position, m_lastBakePosition);
+            if (m_cacheDistance >= m_updateDistance)
+            {
+                Bake();
+            }
+        }
+
+        private void Bake()
+        {
+            if (m_navMeshSurface == null)
+            {
+                return;
+            }
+
+            m_transform.position = m_targetAgent.position;
+            m_lastBakePosition = m_transform.position;
+            m_navMeshSurface.BuildNavMesh();
+        }
+    }
+}
